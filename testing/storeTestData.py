33d1cd9b506ff8bbc3d9162c1d3b8bb0b57156a8
Pavida-M <144598087+Pavida-M@users.noreply.github.com>
2023-11-16 22:06:12 +0700

    Store data for testing in Firestore

---
+++ testing/storeTestData.py
@@ -0,0 +1,29 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import pandas as pd
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate(
+    r"C:\Users\HUAWEI\OneDrive\เดสก์ท็อป\IoT-Dashboard\testing\test-iotdashboard-firebase-adminsdk-1oqvm-14127a112e.json")
+
+firebase_admin.initialize_app(cred)
+
+# Load your time-series dataset
+dataset = pd.read_csv(
+    r"C:\Users\HUAWEI\OneDrive\เดสก์ท็อป\IoT-Dashboard\prepareData\dataset_clean.csv")
+
+# Select the time series dataset between 19:00:00 to 23:59:59 of 1/6/2016 to conduct the experiment
+data = dataset.iloc[414000:432000]
+
+# Store data in the primary collection
+db = firestore.client()
+primary_collection = db.collection("primary")
+
+for _, row in data.iterrows():
+    # Combine date and time to create a unique document ID without nested slashes
+    time_str = f"{row['date']}_{row['time']}".replace("/", "-")
+
+    # Use the time_str as the document ID
+    primary_collection.document(time_str).set(row.to_dict())
+
+print("Data storage completed.")
